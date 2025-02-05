e264e87fe2c9715a86e1c92ce51ee345bad64b94
Mehrad Hajati <m.hajati@dal.ca>
2025-02-05 17:50:17 -0400

    Added and documented a copy of the cars dataset.

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Cars Dataset
+#'
+#' The `cars` dataset gives the speed of cars and the distances taken to stop.
+#'
+#' @format A data frame with 50 observations on 2 variables:
+#' \describe{
+#'   \item{speed}{Speed of the car (in mph)}
+#'   \item{dist}{Stopping distance (in ft)}
+#' }
+#' @source Built-in R dataset
+"cars"
+data(cars)
